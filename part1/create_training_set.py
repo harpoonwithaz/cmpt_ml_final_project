c5095d059c388486719f8668c8b5b50a3b4a6bcc
Googleplex888 <laurenzo457@gmail.com>
2025-11-19 14:47:15 -0800

    Update create_training_set.py

--- part1/create_training_set.py
+++ part1/create_training_set.py
@@ -1,6 +1,18 @@
 import random
 from typing import List # For type hinting
 
+list_of_list = []
+inList = []
+
+for i in range(100):
+    inList = []
+    for i in range(3):
+        random_int = random.randint(1,1001)
+        inList.append(random_int)
+    list_of_list.append(inList)
+
+print(list_of_list)
+
 def create_inputs() -> List[List[int]]:
     '''
     Creates a list of 100 lists containing 3 random integers from 0 to 1000
