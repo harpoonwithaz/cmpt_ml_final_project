31a35d54b8a85a9465948ba5f126d5d2cb4af5c9
harpoonwithaz <otad19@gmail.com>
2025-11-23 23:13:28 -0800

    Removed file operation module, added soft-coded output columns

--- part4/main.py
+++ part4/main.py
@@ -1,15 +1,37 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 import os
+import re
 
 # Local module imports
-import file_operation
 from model_performance import calculate_model_performance, graph_error_percentage
 from data_processing import create_training_set, train_test_partition
 
+# Current working directory to ensure compatibility across devices
 cwd = os.getcwd()
+folder_path = f'{cwd}\\part4\\datafiles'
 
-def validate_user_input(prompt, min, max):
+def looks_numeric_forgiving(s: str) -> bool:
+    s = (s or "").strip()
+    if not s:
+        return False
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+def validate_user_input(prompt: str, min: int, max: int) -> int:
+    '''Function to obtain valid numeric based on constrains input from user
+    
+    Args:
+        prompt (str): Message to display to user
+        min (int): Smallest value user can enter
+        max (int): Largest value user can enter
+
+    Returns:
+        user_input (int): User's numeric input
+    '''
     while True:
         user_choice = input(prompt)
         if user_choice.isnumeric():
@@ -21,7 +43,7 @@ def validate_user_input(prompt, min, max):
             print('Error: Enter number')
 
 def print_table(array: list, column_title: str, margin: int = 10):
-    '''Function to print a nice table of values and their indexes
+    '''Function to print a organized table of values and their indexes with justification
     Args:
         array (list): Containing values to print
         column_title (str): Title of the column
@@ -29,35 +51,60 @@ def print_table(array: list, column_title: str, margin: int = 10):
     '''
     print('Number'.ljust(margin), str(column_title))
     for index, item in enumerate(array):
-        print(str(index).ljust(margin), str(item))
+        print(str(index).ljust(margin), str(item.strip('\n')))
 
-# Ask user which file to use
-# Prints the available files in the dedicated folder
-files = file_operation.get_files()
-if files:
+# Obtains all files in specified directory
+print(f'Scanning for files in: {folder_path}')
+files = os.listdir(folder_path)
+# Filters out unnecessary files
+csv_files = [
+    f for f in files
+    if f.lower().endswith(".csv")
+]
+
+if csv_files: # Csv files are in directory
     # Prints available files
-    print(f'{len(files)} item(s) found.')
-    print_table(files, 'File Name')
+    print(f'{len(csv_files)} item(s) found.')
+    print_table(csv_files, 'File Name')
 
     # Asks user for file to use
     print('Which file do you want to use for training?')
-    choice = validate_user_input('Enter number: ', 0, len(files)-1)
-    training_file = f'{cwd}\\part4\\datafiles\\{files[choice]}'
+    choice = validate_user_input('Enter number: ', 0, len(csv_files)-1)
+    print(f'----> {csv_files[choice].upper()}')
+    training_file = f'{cwd}\\part4\\datafiles\\{csv_files[choice]}'
 
-    # Ask for columns to be used in training and for testing
-    # Print out columns
-    file_columns = file_operation.get_columns(training_file)
+    # Opens
+    try:
+        with open(training_file, 'r', encoding='utf-8') as file:
+            header = file.readline()
+            file_columns = header.split(',')
+            row1 = file.readline()
+            sample_row = row1.split(',')
+    except Exception as e:
+        print(f'Error in opening file: {e}')
+    
+    # Prints table containing columns
     print_table(file_columns, 'Column Name')
     
     # Ask for index to include for output data
-    attempts = 1
-    while attempts >= 0:
-        print('Which column do you want to use as output data for the model?')
-        choice = validate_user_input('Enter number:', 0, len(file_columns)-1)
-        output_index = 1
-        # I ENDED HERE. MAKE A CHECK FOR IF THE FEATURE CONTAINS A STR
+    print('Which column do you want to use as output data for the model?')
+    while True:
+        choice = validate_user_input('Enter number: ', 0, len(file_columns)-1)
+        # Check to make sure the chosen column doesn't contain a str
+        column_sample = sample_row[choice].strip('\n')
+        print(f'CHOICE EXAMPLE "{column_sample}"')
+        if not looks_numeric_forgiving(column_sample):
+            print('Error: The row must contain a numeric value')
+        else:
+            print(f'----> {file_columns[choice].upper()}')
+            output_index = choice
+            break
 
-    # Ask for indexes to include for input data
+    # Ask for index to include for input data
+    attempts = len(file_columns) - 1
+    while attempts:
+        ## ENDED OFF HERE
+        pass
     input_indexes = [2,3,4,5,6,7,8,9]
     # Stop when done
 
@@ -83,7 +130,8 @@ if files:
     # Output results/performance visually
     error_percentages = calculate_model_performance(actual_values=y_test, predicted_values=prediction)
     graph_error_percentage(error_percentages, 'Predictions Error Percentages')
-
+else:
+    print(f'No CSV files found in: {folder_path.upper()}')
 # Loop back to main
 #if __name__ == '__main__':
     #main()
