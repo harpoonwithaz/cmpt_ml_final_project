b051a08d2fcefa8ccef5888692e7bccf8581cfd6
Googleplex888 <laurenzo457@gmail.com>
2025-11-26 13:27:26 -0800

    New data processing and read me

--- part3/data_processing.py
+++ part3/data_processing.py
@@ -1,58 +1,46 @@
-# Part 3 
-# Author Laurenzo Maddatu
-# November 24, 2025
+# =====================================
+# Author: Laurenzo Maddatu
+# Date: 11/24/2025
+# Description: Module for processing the data from the csv file into a training set.
+# =====================================
+
 import os
 from typing import List, Tuple
 
 # Predicting a students second semester grades
 
-
-def create_data_set() -> Tuple[List[List[int]], List[int]]:
+def create_data_set() -> Tuple[List[List[float]], List[float]]:
 
     # Ensures python can find the correct file location
     cwd = os.getcwd()
-    file_location = f'{cwd}\\part3\\data\\studentPerformance.csv'
+    file_location = os.path.join(cwd, 'part3', 'data', 'studentPerformance.csv')
 
     with open(file_location, "r" , encoding="utf-8") as file:
 
-        # Csv file indexes to not include in input data, 31 is the answer and 30 is the approved list, which would make the prediction too easy 
-        elements_to_skip = (30,31)
+        # Csv file indexes to not include (refer to README.md for explanations)
+        elements_to_skip = (29,30,31,32,36)
 
         output_data = []
         input_data = []
 
-        # Number corresponding to graduate, enrolled, or dropout
-        enrollment = {
-            "Graduate": 1.0,
-            "Enrolled": 2.0,
-            "Dropout" : 3.0
-        }
-
-        #for line in file.readlines():
-        #  print line
         header = file.readline()
 
         lines = file.readlines()
         for line in lines:
             information = line.strip("\n").split(";")
             
+            # Getting the list of output data
             grade_value = float(information[31])
             output_data.append(grade_value)
-        
+
+            # Creating the list of lists for the input data
             row_inputs = []
             for i in range(len(information)):
                 if i not in elements_to_skip:
-                    # Checking this index specifically because it contains strings, need these lines to use the dictionary from earlier
-                    if i == 36:
-                        status = information[i].strip()
-                        if status in enrollment:
-                            row_inputs.append(enrollment[status])
-                    else:
-                        row_inputs.append(float(information[i]))
+                    row_inputs.append(float(information[i]))
             input_data.append(row_inputs)
 
     return input_data, output_data
-
 def train_test_partition(data: list) -> Tuple[List, List]:
     '''Function to partition data into training data and test data
 
